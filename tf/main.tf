4e0ab1d929d8f34ba7c48d8e2b42304456c46023
mfranz <franz08@email.franklin.edu>
2022-12-28 11:45:39 -0500

    changes to terraform stuffs

--- tf/main.tf
+++ tf/main.tf
@@ -1,38 +1,20 @@
-# ----------------------------------------------------
-# configure aws provider
-# ----------------------------------------------------
-provider "aws" {
-  region  = var.region
-}
-
-# ----------------------------------------------------
-# create dynamo db for deployments locking 
-# ----------------------------------------------------
-resource "aws_dynamodb_table" "terraform_statelock" {
-  name           = local.ddb_name
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-# ----------------------------------------------------
-# create S3 bucket that will be used for the backend
-# ----------------------------------------------------
-resource "aws_s3_bucket" "remote_state" {
-  bucket        = local.state_bucket_name
-  force_destroy = local.destroy_bucket
-  acl           = "authenticated-read"
-
-  versioning {
-    enabled = true
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  
+    }
   }
 
-  tags = {
-    Env = local.env
+  required_version = "~> 1.3.6"
+
+  backend "s3" {
+    bucket = "personal-project-remote-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "personal-project-tf-statelock"
+    encrypt        = true
   }
+
 }
